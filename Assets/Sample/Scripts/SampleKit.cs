59fa546433e2af1194b3b78748905b974b6f7186
psmjazz <psmjazz96@gmail.com>
2024-03-13 16:42:07 +0900

    MessageHolder -> Channel 변경사항 적용

--- Assets/Sample/Scripts/SampleKit.cs
+++ Assets/Sample/Scripts/SampleKit.cs
@@ -16,23 +16,21 @@ public class SampleKit
         handler.Subscribe("native", OnNative);
     } 
 
-    private void OnTestReturn(MessageHolder messageHolder)
+    private void OnTestReturn(Channel channel)
     {
-        if(messageHolder.Message.Container.TryGetValue("data", out string data))
+        if(channel.Message.Container.TryGetValue("data", out string data))
             Debug.Log("Unity... SampleNode : " + data);
         else
             Debug.Log("Unity... SampleNode : no data");
     }
 
-    private void OnNative(MessageHolder messageHolder)
+    private void OnNative(Channel channel)
     {
-        if(messageHolder.Message.Container.TryGetValue("data", out string data)){
+        if(channel.Message.Container.TryGetValue("data", out string data)){
             Debug.Log("Unity... Native : " + data);
 
-            // Message reply = new Message("nativeReturn");
-            // reply.Envelope.SenderID = handler.ID;
-            // reply.Envelope.ReceiverID = messageHolder.Message.Envelope.SenderID;
-            // messageHolder.Reply(reply);
+            Message reply = new Message("nativeReturn");
+            channel.Reply(reply);
         }
         else
             Debug.Log("Unity... Native : no data");
@@ -43,13 +41,11 @@ public class SampleKit
         Container container1 = new Container();
         container1.Add("data", "first");
         Message message1 = new Message("test", container1);
-        message1.Envelope.SenderID = handler.ID;
         handler.Publish(message1, Tag.Native);
         
         Container container2 = new Container();
         container2.Add("data", "second");
         Message message2 = new Message("testRecall", container2);
-        message2.Envelope.SenderID = handler.ID;
         handler.Publish(message2, Tag.Native);
     }
 }
